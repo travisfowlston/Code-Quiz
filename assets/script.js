54ebc47372f3ccb09b59e173af1c82084b867c56
Travis Fowlston <travisfowlston@ymail.com>
2023-10-16 16:53:55 -0700

    added more to HTML, CSS, and JS files

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+/* GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score */
+
+/* Created an array of questions for the quiz */
+var theQuestions = [
+    {
+        question: 'JavaScript is which type of language?',
+        a: 'Programming',
+        b: 'Spanish',
+        c: 'English',
+        d: 'French',
+        answer: 'a'
+    },
+    {
+        question: 'JavaScript is which type of language?',
+        a: 'Programming',
+        b: 'Spanish',
+        c: 'English',
+        d: 'French',
+        answer: 'a'
+    },
+    {
+        question: 'JavaScript is which type of language?',
+        a: 'Programming',
+        b: 'Spanish',
+        c: 'English',
+        d: 'French',
+        answer: 'a'
+    },
+    {
+        question: 'JavaScript is which type of language?',
+        a: 'Programming',
+        b: 'Spanish',
+        c: 'English',
+        d: 'French',
+        answer: 'a'
+    },
+    {
+        question: 'JavaScript is which type of language?',
+        a: 'Programming',
+        b: 'Spanish',
+        c: 'English',
+        d: 'French',
+        answer: 'a'
+    },
+]
